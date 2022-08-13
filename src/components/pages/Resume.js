9319debcd4f9a668ceecf56d2a44a3851107c20b
Tye Stanley <tyestanley090@gmail.com>
2022-08-13 15:08:03 -0700

    added resume components and updated with downloadable resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,14 +4,23 @@ export default function Resume() {
   return (
     <div>
       <h2>Resume</h2>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <p className="m-4">You can download my resume by clicking "My Resume" </p>
+      <a className="btn btn-primary" href='src\assets\resumePDF\myResume.pdf' download>
+        My Resume
+      </a>
+      <div>
+        <h4 className="mt-4">My Developer Proficiencies</h4>
+        <ul>
+          <li className="mt-4">JavaScript</li>
+          <li className="mt-1">React</li>
+          <li className="mt-1">Express</li>
+          <li className="mt-1">MongoDB</li>
+          <li className="mt-1">Node</li>
+          <li className="mt-1">MySql</li>
+          <li className="mt-1">Many Others...</li>
+        </ul>
+      </div>
+      
     </div>
   )
 }
