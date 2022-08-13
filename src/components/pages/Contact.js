4da545e86d881c4ee1b41fd6f250130474d27452
Tye Stanley <tyestanley090@gmail.com>
2022-08-13 11:55:18 -0700

    added contact form page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
-export default function Contact() {
+export default function ContactForm() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' })
+  const { name, email, message } = formState;
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function handleChange(e) {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        if (!e.target.value.length) {
+          setErrorMessage(`${e.target.name} is required.`);
+        } else {
+          setErrorMessage('');
+        }
+      }
+    }
+    if (e.target.name === 'name' || 'message') {
+      if (!e.target.value) {
+        setErrorMessage(`Your ${e.target.name} is required.`)
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({...formState, [e.target.name]: e.target.value })
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
   return (
-    <div>
-      <h2>Contact</h2>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
-  )
+    <section id="contactId" className="d-flex flex-column">
+      <h1>Contact Me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input className="form-control" type="text" defaultValue={name} name="name" onBlur={handleChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
+          <input className="form-control" type="email" defaultValue={email} name="email" onBlur={handleChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea className="form-control" name="message" defaultValue={message} onBlur={handleChange} rows="5" />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
